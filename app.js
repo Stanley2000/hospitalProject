fee22206f69827f0250ea3cb9058c1e6ba640e3b
Stanley2000 <stanleykelechi2000@gmail.com>
2024-03-05 17:53:02 +0100

    debuggin on the live server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const userRoute = require("./routers/userRoutes");
 const globalErrorController = require("./controllers/errorController");
 const authRoute = require("./routers/authRoutes");
+const AppError = require("./utils/appError");
 
 const app = express();
 
@@ -13,6 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use('/api/v1/login')
 app.use("/api/v1/staff", authRoute);
 app.use("/api/v1/users", userRoute);
+app.use("*", (req, res, next) => {
+  next(new AppError("This route is not in my server", 404));
+});
 app.use(globalErrorController);
 
 module.exports = app;
