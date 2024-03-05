4fcc77399d64ae7ad3d0455823aedbe2807dd6c9
Stanley2000 <stanleykelechi2000@gmail.com>
2024-03-05 16:46:54 +0100

    updated the env file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
-const port = process.env.port;
+const port = process.env.PORT || 5000;
 const DB = process.env.DATABASE;
 mongoose.connect(DB).then((con) => {
   console.log("Connected to the DATABASE");
